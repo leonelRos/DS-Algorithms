52f5ad847dc6841692acb2ef3284c9f626f630af
leonelros <rleonel426@gmail.com>
2020-09-10 21:05:08 -0700

    more challenges

--- Frequency_Counter.js
+++ Frequency_Counter.js
@@ -22,22 +22,20 @@
  */
 
 function unique(arr) {
-    let newArr = [];
-    let counter= {}
-  for(let i = 0; i < arr.length; i++){
-    let num = arr[i]
-    counter[num] ? counter[num] += 1 : counter[num] = 1
-   console.log(counter)
+  let newArr = [];
+  let counter = {};
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+    counter[num] ? (counter[num] += 1) : (counter[num] = 1);
+    console.log(counter);
   }
-  for (let key in counter){
-    newArr.push(parseInt(key))
+  for (let key in counter) {
+    newArr.push(parseInt(key));
   }
-  return newArr
-  
-  }
-  
-  console.log(unique([1, 2, 4, 4, 5, 6]))
+  return newArr;
+}
 
+console.log(unique([1, 2, 4, 4, 5, 6]));
 
 /**
  * Word Count
@@ -65,16 +63,47 @@ function unique(arr) {
  */
 
 function wordCount(sentence) {
-// YOUR WORK HERE
-let newStr =  sentence.toLowerCase().replace(/[,.!]/g,'').split(' ')
+  // YOUR WORK HERE
+  let newStr = sentence.toLowerCase().replace(/[,.!]/g, "").split(" ");
 
-let counter = {};
-for(let i = 0; i < newStr.length; i++){
-    let word = newStr[i]
-    counter[word] ? counter[word] += 1 : counter[word] =1;
+  let counter = {};
+  for (let i = 0; i < newStr.length; i++) {
+    let word = newStr[i];
+    counter[word] ? (counter[word] += 1) : (counter[word] = 1);
+  }
+  return counter;
 }
-return counter
+console.log(wordCount("the cat and the hat."));
+console.log(wordCount("As soon as possible."));
+console.log(wordCount("Its a man, its a plane, its superman!"));
+
+//FREQUENCY COUNTER
+//write a function that takes 2 arrays and checks if the second array contains the square of each element from the first arrayd regardless of the order.
+//return true if correct or false otherwise. also, if both arrays are different in sizes, return false.
+
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+  for (let key in frequencyCounter1) {
+    if (!(key * key in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter2[key * key] !== frequencyCounter1[key]) {
+      return false;
+    }
+  }
+  console.log(frequencyCounter1);
+  console.log(frequencyCounter2);
+  return true;
 }
-console.log(wordCount('the cat and the hat.'))
-console.log(wordCount('As soon as possible.'))
-console.log(wordCount('Its a man, its a plane, its superman!'))
+
+console.log(same([1, 3, 9, 1], [1, 1, 81, 9]));
+console.log(same([1, 3, 9], [2, 6, 9, 81]));
+console.log(same([1, 3, 9, 10], [2, 6, 9, 81]));
