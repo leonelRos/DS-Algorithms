1bfda7a237db907670d3042eddbcc76038d8e98e
leonelros <rleonel426@gmail.com>
2020-09-29 15:29:47 -0700

    inserting key values into our buckets

--- Data_Structures/HashTables.js
+++ Data_Structures/HashTables.js
@@ -35,6 +35,43 @@ class HashTable {
     }
     this.keyMap[index].push([key, value]);
   }
+  get(key) {
+    let index = this.hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+  keys() {
+    let keysArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!keysArr.includes(this.keyMap[i][j][0])) {
+            keysArr.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return keysArr;
+  }
+  values() {
+    let valArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!valArr.includes(this.keyMap[i][j][1])) {
+            valArr.push(this.keyMap[i][j][1]);
+          }
+        }
+      }
+    }
+    return valArr;
+  }
 }
 let ht = new HashTable(17);
 ht.set("maroon", "#800000");
