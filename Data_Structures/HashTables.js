f5185903e67f111f88315b3200365c73a1137ac4
leonelros <rleonel426@gmail.com>
2020-10-01 16:12:06 -0700

    cows and bulls game

--- Data_Structures/HashTables.js
+++ Data_Structures/HashTables.js
@@ -131,3 +131,55 @@ function twoStrings(s1, s2) {
 }
 console.log(twoStrings("helloo", "yellow")); //yes
 console.log(twoStrings("hi", "world")); //NO
+
+/*
+You are playing the Bulls and Cows game with your friend.
+You write down a secret number and ask your friend to guess what the number is.
+ When your friend makes a guess, you provide a hint with the following info:
+The number of "bulls", which are digits in the guess that are in the correct position.
+The number of "cows", which are digits in the guess that are in your secret number but are located in the wrong position.
+ Specifically, the non-bull digits in the guess that could be rearranged such that they become bulls.
+Given the secret number secret and your friend's guess guess, return the hint for your friend's guess.
+The hint should be formatted as "xAyB", where x is the number of bulls and y is the number of cows. Note that both secret and guess may contain duplicate digits.
+
+Example 1:
+
+Input: secret = "1807", guess = "7810"
+Output: "1A3B"
+Explanation: Bulls are connected with a '|' and cows are underlined:
+"1807"
+  |
+"7810"
+Example 2:
+
+Input: secret = "1123", guess = "0111"
+Output: "1A1B"
+Explanation: Bulls are connected with a '|' and cows are underlined:
+"1123"        "1123"
+  |      or     |
+"0111"        "0111"
+Note that only one of the two unmatched 1s is counted as a cow since the non-bull digits can only be rearranged to allow one 1 to be a bull.
+*/
+
+var getHint = function (secret, guess) {
+  let bulls = 0;
+  let cows = 0;
+  let counter = new Array(10).fill(0);
+  for (let i = 0; i < secret.length; i++) {
+    let s = secret.charAt(i);
+    let g = guess.charAt(i);
+    console.log(s);
+    console.log(g);
+
+    if (s == g) {
+      bulls++;
+    } else {
+      if (counter[s] < 0) cows++;
+      if (counter[g] > 0) cows++;
+      counter[s]++;
+      counter[g]--;
+    }
+  }
+  //  return bulls + "A" + cows + "B"
+  return `${bulls}A${cows}B`;
+};
