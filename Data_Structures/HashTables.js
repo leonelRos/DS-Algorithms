4afb4a7b858f323ec006665b7966f0e31bcca62d
leonelros <rleonel426@gmail.com>
2020-10-05 22:25:36 -0700

    adding hashtable

--- Data_Structures/HashTables.js
+++ Data_Structures/HashTables.js
@@ -214,3 +214,29 @@ var lengthOfLongestSubstring = function (str) {
   }
   return res;
 };
+
+/*
+Given an array of integers nums.
+A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+Return the number of good pairs.
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+Example 2:
+
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+*/
+
+var numIdenticalPairs = function (nums) {
+  let count = 0;
+  let hash = new Array(101).fill(0);
+  for (let num of nums) {
+    count = count + hash[num]++; //the count increases when element in the num has repetitve elements.
+  }
+  return count;
+};
