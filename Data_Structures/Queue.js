3bf95f8e5989aafe6b4d7cae3e22b7f4bf66526f
leonelros <rleonel426@gmail.com>
2020-11-10 20:53:28 -0800

    adding queues DS

--- Data_Structures/Queue.js
+++ Data_Structures/Queue.js
@@ -1,3 +1,13 @@
+/*
+Queues exist everywhere! Think about the last time you waited in line....
+Background tasks
+Uploading resources
+Printing / Task processing
+
+A FIFO data structure!
+First In First Out
+*/
+
 class Node {
   //create my node
   constructor(value) {
@@ -13,6 +23,14 @@ class Queue {
     this.last = null;
     this.size = 0;
   }
+  /*
+  Enqueue Pseudocode
+This function accepts some value
+Create a new node using that value passed to the function
+If there are no nodes in the queue, set this node to be the first and last property of the queue
+Otherwise, set the next property on the current last to be that node, and then set the last property of the queue to be that node
+Increment the size of the queue by 1
+  */
   enqueue(val) {
     var newNode = new Node(val);
     if (!this.first) {
@@ -24,6 +42,15 @@ class Queue {
     }
     return ++this.size;
   }
+  /*
+  Dequeue pseudocode
+If there is no first property, just return null
+Store the first property in a variable
+See if the first is the same as the last (check if there is only 1 node). If so, set the first and last to be null
+If there is more than 1 node, set the first property to be the next property of first 
+Decrement the size by 1
+Return the value of the node dequeued
+  */
   dequeue() {
     if (!this.first) return null;
     var temp = this.first;
