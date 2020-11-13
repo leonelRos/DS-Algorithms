2e944526bbc59d1006b6d4b8223f259bab6084d2
leonelros <rleonel426@gmail.com>
2020-11-12 18:36:29 -0800

    add BST

--- Data_Structures/BinarySearchTree.js
+++ Data_Structures/BinarySearchTree.js
@@ -1,5 +1,22 @@
 /*
+TREE TERMINOLOGY
+Root - The top node in a tree.
+Child -A node directly connected to another node when moving away from the Root.
+Parent - The converse notion of a child.
+Siblings -A group of nodes with the same parent.
+Leaf - A node with no children.
+Edge - The connection between one node and another.
 
+HOW BSTS WORK
+Every parent node has at most two children
+Every node to the left of a parent node is always less than the parent
+Every node to the right of a parent node is always greater than the parent
+
+Big O of BST
+Insertion - O(log n)
+Searching - O(log n)
+NOT guaranteed! 
+Because there could be a linear tree such as 12-->15-->19-->20-->62 in this tree, the time for search or insert is linear O(n)
 */
 class Node {
   constructor(value) {
