38b67344d61d4e32b1cf00808572c53f83142e00
leonelros <rleonel426@gmail.com>
2020-11-12 18:39:16 -0800

    add BST

--- Data_Structures/BinarySearchTree.js
+++ Data_Structures/BinarySearchTree.js
@@ -30,6 +30,23 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  /*
+  INSERTING A NODE
+Steps - Iteratively or Recursively
+
+Create a new node
+Starting at the root
+Check if there is a root, if not - the root now becomes that new node!
+If there is a root, check if the value of the new node is greater than or less than the value of the root
+If it is greater 
+Check to see if there is a node to the right
+If there is, move to that node and repeat these steps
+If there is not, add that node as the right property
+If it is less
+Check to see if there is a node to the left
+If there is, move to that node and repeat these steps
+If there is not, add that node as the left property
+  */
   insert(value) {
     var newNode = new Node(value);
     if (this.root === null) {
@@ -56,6 +73,22 @@ class BinarySearchTree {
       }
     }
   }
+
+  //   Finding a Node in a BST
+  // Steps - Iteratively or Recursively
+
+  // Starting at the root
+  // Check if there is a root, if not - we're done searching!
+  // If there is a root, check if the value of the new node is the value we are looking for. If we found it, we're done!
+  // If not, check to see if the value is greater than or less than the value of the root
+  // If it is greater
+  // Check to see if there is a node to the right
+  // If there is, move to that node and repeat these steps
+  // If there is not, we're done searching!
+  // If it is less
+  // Check to see if there is a node to the left
+  // If there is, move to that node and repeat these steps
+  // If there is not, we're done searching!
   find(value) {
     if (this.root === null) return false;
     var current = this.root;
@@ -72,6 +105,8 @@ class BinarySearchTree {
     if (!found) return undefined;
     return current;
   }
+
+  //this code works as well as find but return boolean true or false instead.
   // contains(value){
   //   if(this.root === null) return false;
   //   var current = this.root;
