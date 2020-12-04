b07cd13bb95f8c3f0ecdfb8f53e33a80f86af914
leonelros <rleonel426@gmail.com>
2020-12-03 20:21:23 -0800

    nothing to commit

--- Data_Structures/BinarySearchTree.js
+++ Data_Structures/BinarySearchTree.js
@@ -287,9 +287,7 @@ var rangeSumBST = function (root, low, high) {
 };
 
 //Maximum Depth of Binary Tree:
-
 // Given the root of a binary tree, return its maximum depth.
-
 // A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
 // //                           3             1
