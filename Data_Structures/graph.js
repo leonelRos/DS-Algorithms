487e7c313a5fbd9f7de33c34ae1262f131c4ea8a
leonelros <rleonel426@gmail.com>
2020-12-15 20:16:32 -0800

    adding grapg

--- Data_Structures/graph.js
+++ Data_Structures/graph.js
@@ -1,3 +1,15 @@
+// USES FOR GRAPHS
+// Social Networks
+// Location / Mapping
+// Routing Algorithms
+// Visual Hierarchy
+// File System Optimizations
+// EVERYWHERE!
+
+// WHAT ARE GRAPHS
+// A graph data structure consists of a finite (and possibly mutable) set of vertices or nodes or points,
+// together with a set of unordered pairs of these vertices for an undirected graph or a set of ordered pairs for a directed graph.
+
 class Graph {
   constructor() {
     this.adjacencyList = {};
@@ -14,6 +26,29 @@ class Graph {
   addEdge(vertex1, vertex2) {
     this.adjacencyList[vertex1].push(vertex2);
     this.adjacencyList[vertex2].push(vertex1);
+  }
+  //   This function should accept two vertices, we'll call them vertex1 and vertex2
+  // The function should reassign the key of vertex1 to be an array that does not contain vertex2
+  // The function should reassign the key of vertex2 to be an array that does not contain vertex1
+  // Don't worry about handling errors/invalid vertices
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter((v) => {
+      v !== vertex2;
+    });
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter((v) => {
+      v !== vertex1;
+    });
+  }
+  //   The function should accept a vertex to remove
+  // The function should loop as long as there are any other vertices in the adjacency list for that vertex
+  // Inside of the loop, call our removeEdge function with the vertex we are removing and any values in the adjacency list for that vertex
+  // delete the key in the adjacency list for that vertex
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
   }
 }
 
