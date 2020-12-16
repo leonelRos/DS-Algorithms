8b09ba11c402e4b2eebc4c9f927a1a3fd54ba6d3
leonelros <rleonel426@gmail.com>
2020-12-15 19:50:23 -0800

    adding grapg

--- Data_Structures/graph.js
+++ Data_Structures/graph.js
@@ -7,6 +7,14 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) return (this.adjacencyList[vertex] = []);
   }
+  //   This function should accept two vertices, we can call them vertex1 and vertex2
+  // The function should find in the adjacency list the key of vertex1 and push vertex2 to the array
+  // The function should find in the adjacency list the key of vertex2 and push vertex1 to the array
+  // Don't worry about handling errors/invalid vertices
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
 }
 
 let g = new Graph();
@@ -21,4 +29,3 @@ g.addEdge("Hong Kong", "Tokyo");
 g.addEdge("Hong Kong", "Dallas");
 g.addEdge("Los Angeles", "Hong Kong");
 g.addEdge("Los Angeles", "Aspen");
-console.log(g);
