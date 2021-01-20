4085c37454a7bfa6b2446dedc0a26bafc697e9da
leonelros <rleonel426@gmail.com>
2021-01-19 16:04:28 -0800

    adding graph practice

--- Data_Structures/graph.js
+++ Data_Structures/graph.js
@@ -51,6 +51,27 @@ class Graph {
     delete this.adjacencyList[vertex];
   }
 
+  //Graph traversal
+
+  //          A
+  //        /   \
+  //       B     C
+  //       |     |
+  //       D --- E
+  //        \   /
+  //          F
+
+  //   The function should accept a starting node
+  // Create a list to store the end result, to be returned at the very end
+  // Create an object to store visited vertices
+  // Create a helper function which accepts a vertex
+  // The helper function should return early if the vertex is empty
+  // The helper function should place the vertex it accepts into the visited object and push that vertex into the result array.
+  // Loop over all of the values in the adjacencyList for that vertex
+  // If any of those values have not been visited, recursively invoke the helper function with that vertex
+  // Invoke the helper function with the starting vertex
+  // Return the result array
+
   depthFirstRecursive(start) {
     const result = [];
     const visited = {};
@@ -123,3 +144,19 @@ g.addEdge("Hong Kong", "Tokyo");
 g.addEdge("Hong Kong", "Dallas");
 g.addEdge("Los Angeles", "Hong Kong");
 g.addEdge("Los Angeles", "Aspen");
+
+//this part works with graph traversal
+let graphTraversal = new Graph();
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+g.depthFirstRecursive("A");
