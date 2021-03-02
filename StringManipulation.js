2acc30c7336cb9dba600a1e1c42efde5e14b64e4
leonelros <rleonel426@gmail.com>
2021-03-01 21:03:03 -0800

    adding new challenge

--- StringManipulation.js
+++ StringManipulation.js
@@ -9,4 +9,28 @@ For each test case, print the minimum number of deletions required.
 Constraints
 1≤T≤10
 1≤ length of string ≤105
+
+AAAA ⟹ A, 3 deletions
+BBBBB ⟹ B, 4 deletions
+ABABABAB ⟹ ABABABAB, 0 deletions
+BABABA ⟹ BABABA, 0 deletions
+AAABBB ⟹ AB, 4 deletions
+
+Check ipad for pseudocode
 */
+
+function alternatingCharacters(s) {
+  let deletions = 0;
+  for (let i = 0; i < s.length; i++) {
+    let left = s[i];
+    let right = s[i + 1];
+    if (left === right) {
+      deletions += 1;
+      right++;
+    } else {
+      left = right;
+      right++;
+    }
+  }
+  return deletions;
+}
