5649f87c077db509dbcdbd36fc15b2e589fd50a0
leonelros <rleonel426@gmail.com>
2021-04-27 14:25:36 -0700

    new array problem

--- Data_Structures/Arrays.js
+++ Data_Structures/Arrays.js
@@ -112,3 +112,44 @@ function EquivalentKeypresses(strArr) {
   return checkKeyPressed(strArr[0]) === checkKeyPressed(strArr[1]);
 }
 console.log(EquivalentKeypresses(["a,b,c,d", "a,b,c,d,-B,d"]));
+
+/*
+Have the function ArithGeo(arr) take the array of numbers stored in arr and return the string "Arithmetic" if the sequence follows an arithmetic pattern 
+or return "Geometric" if it follows a geometric pattern. If the sequence doesn't follow either pattern return -1. 
+An arithmetic sequence is one where the difference between each of the numbers is consistent, 
+where as in a geometric sequence, each term after the first is multiplied by some constant or common ratio. 
+Arithmetic example: [2, 4, 6, 8] and Geometric example: [2, 6, 18, 54]. Negative numbers may be entered as parameters, 
+0 will not be entered, and no array will contain all the same elements.
+
+Use the Parameter Testing feature in the box below to test your code with different arguments.
+*/
+
+function ArithGeo(arr) {
+  const isArithmetic = (num) => {
+    let consistent = num[1] - num[0];
+    for (let i = 0; i < num.length - 1; i++) {
+      if (arr[i + 1] - arr[i] !== consistent) {
+        return false;
+      }
+    }
+    return true;
+  };
+  const isGeometric = (num) => {
+    let constant = num[1] / num[0];
+    for (let i = 0; i < num.length - 1; i++) {
+      if (arr[i + 1] / arr[i] !== constant) {
+        return false;
+      }
+    }
+    return true;
+  };
+  if (isArithmetic(arr)) {
+    return "Arithmetic";
+  } else if (isGeometric(arr)) {
+    return "Geometric";
+  } else {
+    return -1;
+  }
+}
+console.log(ArithGeo([2, 4, 6, 8]));
+console.log(ArithGeo([2, 6, 18, 54]));
