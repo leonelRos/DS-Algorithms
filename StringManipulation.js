7010c46f2b97d73ca7ac0f249e1d041addc94fd1
leonelros <rleonel426@gmail.com>
2021-04-27 17:24:02 -0700

    new array problem

--- StringManipulation.js
+++ StringManipulation.js
@@ -79,3 +79,23 @@ function HTMLElements(str) {
 }
 
 console.log(HTMLElements("<div><div><b>Hello</b></div></p>"));
+
+/*
+Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string. 
+If there are two or more words that are the same length, return the first word from the string with that length. 
+Ignore punctuation and assume sen will not be empty. Words may also contain numbers, for example "Hello world123 567"
+*/
+
+function LongestWord(sen) {
+  let cleanStr = sen.replace(/[^\w\s]/gi, "").split(" ");
+  let counter = cleanStr[0];
+  for (let i = 0; i < cleanStr.length; i++) {
+    if (cleanStr[i].length > counter.length) {
+      counter = cleanStr[i];
+    }
+  }
+  return counter;
+}
+
+console.log(LongestWord("fun&!! time losandd"));
+console.log(LongestWord("funlko&!! time"));
